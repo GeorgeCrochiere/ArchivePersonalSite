01355932803cedfe8c1b52de3d1d39c34314e223
George D. Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2022-07-14 18:28:10 -0400

    Optimized menu change based on screen size

--- js/site.js
+++ js/site.js
@@ -2,16 +2,16 @@ headerSizetemp();
 
 
 function headerSizetemp() {
-    var width = window.outerWidth;
+    var width = document.getElementById("html").offsetWidth;
     var homeWidth = document.getElementById("homeButton").offsetWidth;
     if (width > (homeWidth * 4)) {
         document.getElementById("main").style.backgroundColor = "crimson";
-        document.getElementById("navbar").style.display = "block";
+        document.getElementById("navbar").style.display = "flex";
         document.getElementById("collapse-navbar").style.display = "none";
     } else {
         document.getElementById("main").style.backgroundColor = "blueviolet";
         document.getElementById("navbar").style.display = "none";
-        document.getElementById("collapse-navbar").style.display = "block";
+        document.getElementById("collapse-navbar").style.display = "flex";
     }
 
     var txt = "Home Width: " + homeWidth + ", Window Width: " + width;
